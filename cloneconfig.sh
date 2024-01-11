23be9d20a8e1998bbef678d796efc1ca1ec192e7
Stabby <0xStabby@protonmail.com>
2024-01-11 14:15:55 -0600

    another attempt with the command that did work before

--- cloneconfig.sh
+++ cloneconfig.sh
@@ -6,9 +6,7 @@ git checkout master -f
 git branch --set-upstream-to origin/master
 git submodule update --init --recursive
 
-sudo bash -C 'cat <<EOL >> /etc/bash.bashrc
-export XDG_CONFIG_HOME="$HOME/.config"
-export XDG_STATE_HOME="$HOME/.local/state"
-export HISTFILE="$XDG_STATE_HOME/bash/history"
-source "$XDG_CONFIG_HOME/bash/.bash_profile"
-EOL'
+sudo -S echo 'export XDG_CONFIG_HOME="$HOME/.config"' >> /etc/bash.bashrc
+sudo -S echo 'export XDG_STATE_HOME="$HOME/.local/state"' >> /etc/bash.bashrc
+sudo -S echo 'export HISTFILE="$XDG_STATE_HOME/bash/history"' >> /etc/bash.bashrc
+sudo -S echo 'source "$XDG_CONFIG_HOME/bash/.bash_profile"' >> /etc/bash.bashrc
