c2167c36fe12fc8281dadb6c46bfaf2cdfd77203
Stabby <0xStabby@protonmail.com>
2024-01-11 14:14:14 -0600

    attempt to solve sudo command

--- cloneconfig.sh
+++ cloneconfig.sh
@@ -6,9 +6,9 @@ git checkout master -f
 git branch --set-upstream-to origin/master
 git submodule update --init --recursive
 
-sudo cat <<EOL >> /etc/bash.bashrc
+sudo bash -C 'cat <<EOL >> /etc/bash.bashrc
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_STATE_HOME="$HOME/.local/state"
 export HISTFILE="$XDG_STATE_HOME/bash/history"
 source "$XDG_CONFIG_HOME/bash/.bash_profile"
-EOL
+EOL'
