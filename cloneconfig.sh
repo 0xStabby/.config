6f8755cbeec20df2494cd720095e321e71261ca1
Stabby <0xStabby@protonmail.com>
2024-01-11 13:48:09 -0600

    set /etc/bash to look for correct .bashrc, updated submodules

--- cloneconfig.sh
+++ cloneconfig.sh
@@ -1,7 +1,10 @@
- cd ~/.config
- git init
- git remote add origin git@github.com:0xStabby/.config.git
- git pull
- git checkout master -f
- git branch --set-upstream-to origin/master
- git submodule update --init --recursive
+cd ~/.config
+git init
+git remote add origin git@github.com:0xStabby/.config.git
+git pull
+git checkout master -f
+git branch --set-upstream-to origin/master
+git submodule update --init --recursive
+
+# set bash to look in ~/.config/bash instead of home dir
+sudo echo 'source "$XDG_CONFIG_HOME/bash/.bash_profile"' >> /etc/bash.bashrc
