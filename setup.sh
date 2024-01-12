c05c63d98739ee5efa4d04bfdfdb873d7cd09397
Stabby <0xStabby@protonmail.com>
2024-01-11 23:17:44 -0600

    don't move .gvm

--- setup.sh
+++ setup.sh
@@ -98,8 +98,7 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 # get everything I could need for go
 sudo pacman -S bison --noconfirm
 bash < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
-mv ~/.gvm ~/.config/.gvm
-source ~/.config/bash/.bash_profile
+source ~/.gvm/scripts/gvm
 gvm install go1.4
 gvm use go1.4 [--default]
 
@@ -115,3 +114,7 @@ gvm use go1.17.13
 export GOROOT_BOOTSTRAP=$GOROOT
 gvm install go1.20
 gvm use go1.20
+
+# dunno if I can move this yet
+# mv ~/.gvm ~/.config/.gvm
+# source ~/.config/bash/.bash_profile
