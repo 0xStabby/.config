82d8069828a2281f6399a37695106c6efc348095
Stabby <0xStabby@protonmail.com>
2024-01-12 00:10:29 -0600

    install omz non-interactively

--- i3install.sh
+++ i3install.sh
@@ -22,7 +22,9 @@ sudo pacman -S tmux --noconfirm
 
 # install zsh
 sudo pacman -S zsh --needed --noconfirm
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+curl https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o /tmp/install.sh &&
+    sed -i 's/CHSH=no/CHSH=yes/g' /tmp/install.sh &&
+    echo "Y" | sh /tmp/install.sh
 
 sudo echo "[greeter]
    theme-name = Arc-Dark
