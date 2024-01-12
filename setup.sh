5445a3f98f9659158fbbb6b758bd3c96ab4b4894
Stabby <0xStabby@protonmail.com>
2024-01-11 22:41:26 -0600

    source bashrc after installing pnpm

--- setup.sh
+++ setup.sh
@@ -70,6 +70,8 @@ sudo pacman -S nodejs pnpm --noconfirm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+source ~/.config/bash/.bashrc
+source ~/.bashrc
 
 pnpm setup
 
