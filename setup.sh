d71b8a51591bc451b47913bf9fe0e21e814b25bc
Stabby <0xStabby@protonmail.com>
2024-01-12 00:05:26 -0600

    noconfirm Syu

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ read -p "Enter the country for which you want to uncomment servers: " country
 sudo curl -o $mirrorlist_file "https://archlinux.org/mirrorlist/?country=all&protocol=http&protocol=https&ip_version=4"
 sudo sed -i "/^## $country/,/^$/ s/^#Server/Server/" "$mirrorlist_file"
 echo "Servers for $country uncommented in $mirrorlist_file."
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 cd ~/.config
 git init
