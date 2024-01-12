1d1cd2e96912d41dad210a5202c03e3f6c552c4b
Stabby <0xStabby@protonmail.com>
2024-01-12 00:04:15 -0600

    sudo read does not work so, now just read before sudo curl

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 # update mirrorlist
 mirrorlist_file="/etc/pacman.d/mirrorlist"
-# sudo and read in the same command so nothing inbetween the only prompts
-sudo read -p "Enter the country for which you want to uncomment servers: " country
+read -p "Enter the country for which you want to uncomment servers: " country
 sudo curl -o $mirrorlist_file "https://archlinux.org/mirrorlist/?country=all&protocol=http&protocol=https&ip_version=4"
 sudo sed -i "/^## $country/,/^$/ s/^#Server/Server/" "$mirrorlist_file"
 echo "Servers for $country uncommented in $mirrorlist_file."
