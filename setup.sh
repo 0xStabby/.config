15487449dee1725b6183a25940812a073e400a97
Stabby <0xStabby@protonmail.com>
2024-01-14 00:29:48 -0600

    run scripts with sh in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,25 +2,25 @@ source ~/.config/scripts/functions.sh
 
 # Call setup scripts for specific modules
 # update mirror list first to get prompts out of the way
-~/.config/scripts/update_mirrorlist.sh
-~/.config/scripts/clone_config.sh
+sh ~/.config/scripts/update_mirrorlist.sh
+sh ~/.config/scripts/clone_config.sh
 
 # setup submodules
-NO_REBOOT=1 ~/.config/i3/i3install.sh && unset NO_REBOOT
-~/.config/bash/setup.sh
-~/.config/vim/setup.sh
-~/.config/tmux/setup.sh
-~/.config/rofi/setup.sh
-~/.config/zsh/setup.sh
+sh NO_REBOOT=1 ~/.config/i3/i3install.sh && unset NO_REBOOT
+sh ~/.config/bash/setup.sh
+sh ~/.config/vim/setup.sh
+sh ~/.config/tmux/setup.sh
+sh ~/.config/rofi/setup.sh
+sh ~/.config/zsh/setup.sh
 
 # set caps to control permanently
-localectl set-x11-keymap "" "" "" ctrl:nocaps
+sudo localectl set-x11-keymap "" "" "" ctrl:nocaps
 
 # setup dev tools
-~/.config/scripts/setup_js.sh
-~/.config/scripts/setup_rs.sh
-~/.config/scripts/setup_go.sh
-~/.config/scripts/setup_py.sh
+sh ~/.config/scripts/setup_js.sh
+sh ~/.config/scripts/setup_rs.sh
+sh ~/.config/scripts/setup_go.sh
+sh ~/.config/scripts/setup_py.sh
 
 # install crypto stuff
 yay -S frame-eth --noconfirm
